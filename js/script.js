10ab7ef5c600f243c8d83b6e6e2b1592d083eef3
smillkaa <104727784+smillkaa@users.noreply.github.com>
2022-11-20 23:45:31 -0800

    jessica

--- js/script.js
+++
@@ -1,8 +0,0 @@
-const date = new Date()
-const copyright = document.getElementById('copyright')
-
-function writeCopyright() {
-    copyright.innerHTML = "&copy; " + date.getFullYear() + " SELLESTIALL. ALL RIGHTS RESERVED."
-}
-
-writeCopyright()
