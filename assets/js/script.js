b72fb81efd572c581d6ed66408f612b0b3b9f66d
saidananarmo <104727784+smillkaa@users.noreply.github.com>
2023-07-31 01:18:52 +0300

    gallery

--- assets/js/script.js
+++
@@ -1,9 +0,0 @@
-const date = new Date()
-const copyright = document.querySelector('.copyright')
-
-function writeCopyright() {
-    copyright.innerHTML = "&copy; " + date.getFullYear() + " SELLESTIALL. ALL RIGHTS RESERVED."
-}
-
-writeCopyright()
-
