951ef230ec47bd996c4a5a77068503bd2f0faace
saidananarmo <104727784+smillkaa@users.noreply.github.com>
2023-08-20 21:38:55 +0200

    everything works

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,4 @@
-$(document).ready(function(){
-    // Gallery slideshow functionality
-
-    // Paintings array of objects
+$(document).ready(function() {
     const paintings = [
         {
             img: "https://res.cloudinary.com/dyve8u6cx/image/upload/v1690736386/painting1-progressive_rlpti3.jpg",
@@ -39,105 +36,86 @@ $(document).ready(function(){
     ];
 
     let currentIndex = 0;
-    $('.painting').attr('src', paintings[0].img);
-    updatePainting();  // <-- Call the updatePainting function here
-    
-    // Function to update painting img and description when clicked next
+    const $painting = $('.painting');
+    const $description = $('.description');
+    const $poem = $('.poem');
+    const $parent = $('.parent');
+    const $titles = $('.titles');
+
     function updatePainting() {
-        $('.painting').attr('src', paintings[currentIndex].img);
-        $('.description .title').text(paintings[currentIndex].title);
-        $('.description .subtitle').text(paintings[currentIndex].subtitle);
+        const currentPainting = paintings[currentIndex];
+        $painting.attr('src', currentPainting.img);
+        $description.find('.title').text(currentPainting.title);
+        $description.find('.subtitle').text(currentPainting.subtitle);
         
-        var descriptionHtml = "";
-        paintings[currentIndex].description.forEach(paragraph => {
-            descriptionHtml += `<p class="text-start description-text-paragraph">${paragraph}</p>`;
-        });
-        
-        $('.description .description-text').html(descriptionHtml);  // Replace content of gallery-text div with new paragraphs
+        const descriptionHtml = currentPainting.description.map(paragraph => `<p class="text-start description-text-paragraph">${paragraph}</p>`).join('');
+        $description.find('.description-text').html(descriptionHtml);
     }
-    
-    
-
-    // Event listener when clicked next
-    $('.next').click(function() {
-        currentIndex = (currentIndex + 1) % paintings.length; // increment the index and wrap around if needed
-        updatePainting();
-    });
-
-    
-    // Animations
-    
-    // Set the initial opacity and pointer-events for .description
-    $('.description').css({opacity: 0, display: 'none'}).addClass('not-active');
 
     function showDescription() {
-        // Fade in the description div
-        $('.description').removeClass('not-active').css('display', 'block').stop().animate({ opacity: 1 }, 2000);
-
-        // Fade out and slide up the poem and titles div
-        $('.poem').children('.monogram').each(function(i) {
-            $(this).stop().delay(100 * i).animate({ opacity: 0, bottom: '100vh' }, 1000);
+        $description.css('pointer-events', 'auto') // Enable pointer events when showing
+                .removeClass('not-active')
+                .css('display', 'block')
+                .animate({ opacity: 1 }, 2000);
+        $poem.children('.monogram').each(function(i) {
+            $(this).delay(100 * i).animate({ opacity: 0, bottom: '100vh' }, 1000);
         });
-        $('.titles').stop().animate({ opacity: 0 }, 1000);
+        $titles.animate({ opacity: 0 }, 1000);
     }
-
-    // When the element with class showdisplay is clicked
-    $('.showdisplay').click(showDescription);
-
-    // When hovering on the painting, description appears
-    $('.painting, .gallery-text').hover(showDescription);
     
-    $('.exit').click(function() {
-        $('.description').stop().animate({opacity: 0}, 2000, function() {
-            $(this).css('display', 'none').addClass('not-active');
-        });
 
-        // Fade in and slide down the poem and titles div over 1000ms using .animate()
-        $('.poem').children('.monogram').each(function(i) {
-            $(this).stop().delay(100 * i).animate({opacity: 1, bottom: '0px'}, 1000);
-        });
-        $('.titles').stop().animate({opacity: 1}, 300);
-        
-    });
-
-    // Function to show the dim overlay and centered poem
     function showOverlayAndPoem() {
         $('.dim-overlay').css({
             'display': 'block',
             'opacity': 0
-        }).stop(true, true).animate({opacity: 1}, 1000, function() {
+        }).animate({opacity: 1}, 1000, function() {
             $('.poem-centered').css({
                 'display': 'block',
                 'opacity': 0
-            }).stop(true, true).animate({opacity: 1}, 1000);
+            }).animate({opacity: 1}, 1000);
         });
     }
 
-    // Function to hide the dim overlay and centered poem
     function hideOverlayAndPoem() {
-        $('.poem-centered').stop(true, true).animate({opacity: 0}, 1000, function() {
+        $('.poem-centered').animate({opacity: 0}, 1000, function() {
             $(this).css('display', 'none');
-            $('.dim-overlay').stop(true, true).animate({opacity: 0}, 1000, function() {
+            $('.dim-overlay').animate({opacity: 0}, 1000, function() {
                 $(this).css('display', 'none');
             });
         });
     }
 
-    // Click event to show dim overlay and centered poem
-    $('.poem').click(function(event) {
-        event.stopPropagation();
-        showOverlayAndPoem();
+    // Initialize the painting.
+    updatePainting();
+
+    // Event Listeners
+    $('.next').click(function() {
+        currentIndex = (currentIndex + 1) % paintings.length;
+        updatePainting();
     });
 
-        // View note animation
+    $('.painting, .gallery-text').click(showDescription);
 
-   // Delegating the click event of .view-note from .parent
-$('.parent').on('click', '.view-note', function(event) {
-    event.stopPropagation();
-    var elementsToFade = $('.mosquito-poem, .view-note-div');
-    elementsToFade.fadeOut(function(){
-        if ($(this).is(elementsToFade.last())) {
-            const noteContentHtml = `
+
+    $('.exit').click(function() {
+        $description.css('pointer-events', 'none')  // Disable pointer events when hiding
+                .animate({opacity: 0}, 2000, function() {
+                    $(this).css('display', 'none').addClass('not-active');
+                });
+        $poem.children('.monogram').each(function(i) {
+            $(this).delay(100 * i).animate({opacity: 1, bottom: '0px'}, 1000);
+        });
+        $titles.animate({opacity: 1}, 300);
+    });
+    
+
+    $poem.click(showOverlayAndPoem);
+
+    $parent.on('click', '.view-note', function() {
+        const elementsToFade = $('.mosquito-poem, .view-note-div');
+        elementsToFade.fadeOut(function() {
+            if ($(this).is(elementsToFade.last())) {
+                const noteContentHtml = `
                 <div class="note-content mx-auto mt-5 pt-5">
                     <p>As I wrote this poem, I was amused when I read it - sounding so erotic, I thought that the reader must feel like they are being told about a sex scene happening right in its moment.</p>
                     <p>However, the reality of what is being described here is quite different, asexual.</p>
@@ -145,33 +123,27 @@ $('.parent').on('click', '.view-note', function(event) {
                 </div>
                 <p class="close-btn text-center mx-auto mt-5">CLOSE</p>
             `;
-            $('.parent').append(noteContentHtml);
-        }
+                $parent.append(noteContentHtml);
+            }
+        });
     });
-});
 
-// Delegating the click event of .close-btn from .parent
-$('.parent').on('click', '.close-btn', function(event) {
-    console.log('close');
-    event.stopPropagation();
-    hideOverlayAndPoem();
-    const mosquitoPoemHtml = `
-    <div class="mosquito-poem mx-auto col-12 col-md-6">
-    <p class="fst-italic">Mosquito</p>
-    <p class="mb-0">Love.</p>
-    <p class="mb-0">We love and have sex.</p>
-    <p class="mb-0">I was listening to love and sex on a beautiful melodic soundtrack, the conversation was hypnotic, again, as if I was playing a scene in a movie. </p>
-    <p class="mb-0">I spit out my water in my cup, and I said, I ate a mosquito.</p>
-    <p class="mb-0">He said, I looked beautiful spitting.</p>
-</div>
-<div class="view-note-div mx-auto mt-5">
-    <p class="text-center view-note">VIEW NOTE</p>
-</div>`;
-    setTimeout(function() {
-        $('.parent').empty().append(mosquitoPoemHtml);
-    }, 1000);
-});
-
-
-            
+    $parent.on('click', '.close-btn', function() {
+        hideOverlayAndPoem();
+        const mosquitoPoemHtml = `
+            <div class="mosquito-poem mx-auto col-12 col-md-6">
+            <p class="fst-italic">Mosquito</p>
+            <p class="mb-0">Love.</p>
+            <p class="mb-0">We love and have sex.</p>
+            <p class="mb-0">I was listening to love and sex on a beautiful melodic soundtrack, the conversation was hypnotic, again, as if I was playing a scene in a movie. </p>
+            <p class="mb-0">I spit out my water in my cup, and I said, I ate a mosquito.</p>
+            <p class="mb-0">He said, I looked beautiful spitting.</p>
+        </div>
+        <div class="view-note-div mx-auto mt-5">
+            <p class="text-center view-note">VIEW NOTE</p>
+        </div>`;
+        setTimeout(function() {
+            $parent.empty().append(mosquitoPoemHtml);
+        }, 1000);
+    });
 });
